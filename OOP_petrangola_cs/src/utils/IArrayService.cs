0315e19cf1655bdaebb286cea2969a9a605162de
Marco Edoardo Duma <marcoedoardo.duma@studio.unibo.it>
2021-11-05 19:04:33 +0100

    Created IArrayService to split the array of cards

---
+++ OOP_petrangola_cs/src/utils/IArrayService.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+
+namespace OOP_petrangola_cs.src.utils
+{
+    /// <summary>
+    /// Nice way to splite an array using Linq https://stackoverflow.com/a/1841276/13455322
+    /// </summary>
+    public interface IArrayService
+    {
+        static void Split<T>(T[] array, int index, out T[] first, out T[] second)
+        {
+            first = array.Take(index).ToArray();
+            second = array.Skip(index).ToArray();
+        }
+
+        static void SplitMidPoint<T>(T[] array, out T[] first, out T[] second)
+        {
+            Split(array, array.Length / 2, out first, out second);
+        }
+    }
+}
